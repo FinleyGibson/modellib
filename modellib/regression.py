4ebb84ecfbfcf6b870b55ca1d372ceb92c080052
Finley Gibson <f.j.gibson@exeter.ac.uk>
2024-06-12 13:55:38 +0100

    modification to regression

--- modellib/regression.py
+++ modellib/regression.py
@@ -1,7 +1,6 @@
 from modellib.base import Regression
 import numpy.typing as npt
 from multiprocessing import cpu_count
-import xgboost as _xgb
 
 
 class RandomForest(Regression):
@@ -16,13 +15,13 @@ class RandomForest(Regression):
         self.model_parameters = kwargs.copy()
         self.model = self._RandomForestRegressor(**kwargs)
 
-    def _fit(self, x: npt.NDArray[float], y: npt.NDArray[float]):
+    def _fit(self, x: npt.NDArray, y: npt.NDArray):
         if self.dim_out > 1:
             self.model.fit(x, y)
         else:
             self.model.fit(x, y.reshape(-1))
 
-    def _predict(self, x_: npt.NDArray[float]) -> npt.NDArray[float]:
+    def _predict(self, x_: npt.NDArray) -> npt.NDArray:
         return self.model.predict(x_)
 
 
@@ -37,13 +36,13 @@ class LinearRegression(Regression):
         self.model_parameters = kwargs.copy()
         self.model = self._LinearRegression(**kwargs)
 
-    def _fit(self, x: npt.NDArray[float], y: npt.NDArray[float]):
+    def _fit(self, x: npt.NDArray, y: npt.NDArray):
         if self.dim_out > 1:
             self.model.fit(x, y)
         else:
             self.model.fit(x, y.reshape(-1))
 
-    def _predict(self, x_: npt.NDArray[float]) -> npt.NDArray[float]:
+    def _predict(self, x_: npt.NDArray) -> npt.NDArray:
         return self.model.predict(x_)
 
 
@@ -51,17 +50,46 @@ class XGBoost(Regression):
     """
     Wrapper for xgboost regressor
     """
+
+    import xgboost as _xgb
     
     def __init__(self, **kwargs):
         super(XGBoost, self).__init__()
         default_kwargs = {"n_jobs": cpu_count(),
                           "n_estimators": 100}
         default_kwargs.update(kwargs)
-        self.model = _xgb.XGBRegressor(**default_kwargs)
+        self.model = self._xgb.XGBRegressor(**default_kwargs)
         self.model_parameters = default_kwargs
 
-    def _fit(self, x: npt.NDArray[float], y: npt.NDArray[float]):
+    def _fit(self, x: npt.NDArray, y: npt.NDArray):
         self.model.fit(x, y)
 
-    def _predict(self, x_: npt.NDArray[float]):
+    def _predict(self, x_: npt.NDArray):
+        return self.model.predict(x_)
+
+
+class SVM(Regression):
+    """_summary_
+        Wrapper for sklearns Support Vector Machine (SVM)
+    """
+
+    from sklearn import svm as _svm
+
+    def __init__(self, **kwargs):
+        super(SVM, self).__init__()
+        
+        default_kwargs = {
+            "kernel": "rbf"
+            } 
+        default_kwargs.update(kwargs)
+
+        self.model = self._svm.SVR(**kwargs)
+
+    def _fit(self, x: npt.NDArray, y: npt.NDArray):
+        if self.dim_out > 1:
+            self.model.fit(x, y)
+        else:
+            self.model.fit(x, y.reshape(-1))
+
+    def _predict(self, x_: npt.NDArray):
         return self.model.predict(x_)
